b58638d546fb0b0c66c227e8cda3e2b9d5c36a84
agustinustheo <agustinustheoo@gmail.com>
2019-03-02 21:05:08 +0700

    added infund landing page in laravel/views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,59 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>
+            InFund - Where a start happens
+        </title>
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <nav class="infund-nav">
+            <div class="infund-nav-logo"></div>
+            <div class="infund-nav-item-container">
+                <div>Get the app</div>
+                <div>Sign In</div>
+                <div>Sign Up</div>
+            </div>
+        </nav>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <header class="infund-landing">
+            <div class="infund-landing-desc">
+                <div class="infund-logo-text">
+                    InFund
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div style="text-align: center">
+                        Where a Start Happens
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="infund-button">
+                    Start Now
                 </div>
             </div>
-        </div>
+            <div class="infund-landing-background"></div>
+        </header>
+
+        <main class="infund-landing" style="display: flex; align-items: center;">
+            <div style="margin: auto;">
+                <div style="text-align: center">
+                    Feature
+                </div>
+                <div style="text-align: center">
+                    How it Works
+                </div>
+            </div>
+        </main>
+
+        <footer class="infund-landing" style="display: flex; align-items: center;">
+            <div style="margin: auto;">
+                <div style="text-align: center">
+                    quote
+                </div>
+                <div style="text-align: center">
+                    <input type="text" placeholder="Enter email.." style="width: 250px; height: 32px; padding-left: 10px; border-radius: 10px; border: 0; outline: none;">
+                </div>
+                <div class="infund-button">
+                    Start Now
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
